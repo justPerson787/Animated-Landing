c5e9c1fa294542b3133edd161bb6a33e81fd8a65
N.U <n.n.ushakova@gmail.com>
2020-07-10 00:07:18 -0400

    Rearrange JS scripts

---
+++ navbar.js
@@ -0,0 +1,5 @@
+// Navigation
+function toggleNavbar(collapseID) {
+    document.getElementById(collapseID).classList.toggle("hidden");
+    document.getElementById(collapseID).classList.toggle("block");
+}
